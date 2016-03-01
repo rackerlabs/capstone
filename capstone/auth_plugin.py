08b033661f4b69072b12e41c3f77ae6db6fcdd00
David Stanek <dstanek@dstanek.com>
2016-03-01 19:57:39 +0000

    Adds domain checking
    We now check the domain, if provided, against the the domain returned in
    a 'get user' call to Rackspace. This allows us to make sure the correct
    domain is provided.
    
    Change-Id: I8e1994bb23ff2989544e8413b110d562c9d43baf

--- capstone/auth_plugin.py
+++ capstone/auth_plugin.py
@@ -11,6 +11,7 @@
 # under the License.
 
 from keystone import auth
+from keystone import exception
 import requests
 
 from capstone import conf
@@ -22,59 +23,113 @@ METHOD_NAME = 'password'
 
 class RackspaceIdentity(object):
 
+    @classmethod
+    def from_username(cls, username, password,
+                      user_domain_id=None, user_project_id=None):
+
+        return cls(username, password, user_domain_id, user_project_id)
+
+    @classmethod
+    def from_user_id(cls, user_id, password,
+                     user_domain_id=None, user_project_id=None):
+
+        admin_client = cls.from_admin_config()
+        admin_client.authenticate()
+        user_ref = admin_client.get_user(user_id)
+        username = user_ref['username']
+        return cls(username, password, user_domain_id, user_project_id,
+                   user_ref=user_ref)
+
+    @classmethod
+    def from_admin_config(cls):
+        return cls.from_username(conf.admin_username, conf.admin_password)
+
+    def __init__(self, username, password, user_domain_id, user_project_id,
+                 user_ref=None):
+        self._username = username
+        self._password = password
+        self._user_domain_id = user_domain_id
+        self._user_project_id = user_project_id
+        self._user_ref = user_ref
+        self._token = None
+
+    def _assert_user_domain(self, user_id):
+        if not self._user_ref:
+            admin_client = RackspaceIdentity.from_admin_config()
+            admin_client.authenticate()
+            self._user_ref = admin_client.get_user(user_id)
+
+        key = const.RACKSPACE_DOMAIN_KEY
+        if not self._user_ref[key] == self._user_domain_id:
+            raise exception.Unauthorized()
+
+    def authenticate(self):
+        data = {
+            "auth": {
+                "passwordCredentials": {
+                    "username": self._username,
+                    "password": self._password,
+                },
+            },
+        }
+        url = self.get_token_url()
+        resp = requests.post(url, headers=const.HEADERS, json=data)
+        resp.raise_for_status()
+        token_data = resp.json()
+
+        if self._user_domain_id:
+            self._assert_user_domain(token_data['access']['user']['id'])
+
+        self._token = token_data['access']['token']['id']
+        return token_data
+
     def get_user_url(self, user_id):
         return '%s/users/%s' % (conf.rackspace_base_url, user_id)
 
     def get_token_url(self):
         return conf.rackspace_base_url + '/tokens/'
 
-    def get_user_name(self, user_id):
-        token_data = self.authenticate(
-            conf.admin_username, conf.admin_password, conf.admin_project_id)
-        admin_token = token_data['access']['token']['id']
-
+    def get_user(self, user_id):
         headers = const.HEADERS.copy()
-        headers['X-Auth-Token'] = admin_token
+        headers['X-Auth-Token'] = self._token
         resp = requests.get(self.get_user_url(user_id), headers=headers)
         resp.raise_for_status()
-        return resp.json()['user']['username']
-
-    def authenticate(self, username, password, domain_or_project):
-        data = {
-            "auth": {
-                "passwordCredentials": {
-                    "username": username,
-                    "password": password,
-                },
-                "tenantId": domain_or_project,
-            },
-        }
-        resp = requests.post(
-            self.get_token_url(),
-            headers=const.HEADERS,
-            json=data)
-        resp.raise_for_status()
-        return resp.json()
+        return resp.json()['user']
 
 
 class Password(auth.AuthMethodHandler):
 
     def authenticate(self, context, auth_payload, auth_context):
         """Try to authenticate against the identity backend."""
-        domain_id = auth_payload['user'].get('domain', {}).get('id')
-        project_id = auth_payload['user'].get('project', {}).get('id')
-        # TODO(dolph): if (domain_id and project_id), raise a 400
-        domain_or_project = domain_id or project_id
-
-        identity = RackspaceIdentity()
+        user_domain_id = auth_payload['user'].get('domain', {}).get('id')
+        if not user_domain_id:
+            user_domain_id = auth_payload['user'].get('domain', {}).get('name')
+        user_project_id = auth_payload['user'].get('project', {}).get('id')
+        # TODO(dolph): if (user_domain_id and user_project_id), raise a 400
 
         username = auth_payload['user'].get('name')
-        if 'id' in auth_payload['user'] and not username:
-            username = identity.get_user_name(auth_payload['user']['id'])
+        try:
+            if not username:
+                # TODO(dstanek): assert user_domain_id!!
+                identity = RackspaceIdentity.from_user_id(
+                    auth_payload['user']['id'],
+                    auth_payload['user']['password'],
+                    user_domain_id,
+                    user_project_id=user_project_id)
+            else:
+                identity = RackspaceIdentity.from_username(
+                    username,
+                    auth_payload['user']['password'],
+                    user_domain_id=user_domain_id,
+                    user_project_id=user_project_id)
+            token_data = identity.authenticate()
 
-        token_data = identity.authenticate(username,
-                                           auth_payload['user']['password'],
-                                           domain_or_project)
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 401:
+                raise exception.Unauthorized()
+            raise
 
-        auth_context['user_id'] = token_data['access']['user']['id']
+        user_id = token_data['access']['user']['id']
+
+        auth_context['user_id'] = user_id
         auth_context[const.TOKEN_RESPONSE] = token_data
