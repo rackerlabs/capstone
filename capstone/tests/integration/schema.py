fd85004a0fc7b62b76389add4746073be9ee48ab
Jorge Munoz <elkidster@gmail.com>
2016-03-02 20:55:30 +0000

    Keystone should not include Rackspace response
    Add a test to verify that the Rackspace response is not
    included in the Keystone auth response. This patch will
    also include the token schema for response validation.
    
    Change-Id: I9463aafb7feadd00afcc7c0f94d40af02bac7922

---
+++ capstone/tests/integration/schema.py
@@ -0,0 +1,83 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from keystone.common.validation import parameter_types
+
+_domain_properties = {
+    'id': parameter_types.id_string,
+    'name': {'type': 'string'}
+}
+
+_project_properties = {
+    'id': parameter_types.id_string,
+    'name': {'type': 'string'},
+    'domain': {
+        'type': 'object',
+        'properties': _domain_properties,
+        'required': ['id', 'name'],
+        'additonalProperties': False,
+    }
+}
+
+_user_properties = {
+    'id': parameter_types.id_string,
+    'name': {'type': 'string'},
+    'domain': {
+        'type': 'object',
+        'properties': _domain_properties,
+        'required': ['id', 'name'],
+        'additonalProperties': False,
+    }
+}
+
+project_scope_properties = {
+    'audit_ids': {
+        'type': 'array',
+        'items': {
+            'type': 'string',
+        },
+        'minItems': 1,
+        'maxItems': 2,
+    },
+    'expires_at': {'type': 'string'},
+    'issued_at': {'type': 'string'},
+    'methods': {
+        'type': 'array',
+        'items': {
+            'type': 'string',
+        },
+    },
+    'user': {
+        'type': 'object',
+        'required': ['id', 'name', 'domain'],
+        'properties': _user_properties,
+        'additionalProperties': False,
+    },
+    'catalog': {'type': 'array'},
+    'roles': {'type': 'array'},
+    'project': {
+        'type': ['object'],
+        'required': ['id', 'name', 'domain'],
+        'properties': _project_properties,
+        'additionalProperties': False,
+    },
+    'is_domain': parameter_types.boolean,
+}
+
+schema = {
+    'type': 'object',
+    'properties': project_scope_properties,
+    'required': ['audit_ids', 'expires_at', 'issued_at', 'methods',
+                 'user', 'project', 'catalog'],
+    'optional': ['bind'],
+    'additionalProperties': False,
+}
--- capstone/tests/integration/test_integration.py
+++ capstone/tests/integration/test_integration.py
@@ -10,10 +10,13 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import requests
 import testtools
 
+from keystone.common.validation import validators
 from os_client_config import config as cloud_config
-import requests
+
+import schema
 
 
 class IntegrationTests(testtools.TestCase):
@@ -55,6 +58,10 @@ class IntegrationTests(testtools.TestCase):
         headers['X-Auth-Token'] = token_id
         resp = requests.get(url, headers=headers)
         resp.raise_for_status()
+
+    def assertValidTokenResponse(self, resp):
+        validator_object = validators.SchemaValidator(schema.schema)
+        validator_object.validate(resp.json()['token'])
 
     def test_get_v2_token_from_rackspace(self):
         data = {
@@ -164,3 +171,29 @@ class IntegrationTests(testtools.TestCase):
         resp.raise_for_status()
         token = resp.headers['X-Subject-Token']
         self.assertTokenIsUseable(token)
+
+    def test_get_v3_token_does_not_return_rackspace_response(self):
+        data = {
+            "auth": {
+                "identity": {
+                    "methods": ["password"],
+                    "password": {
+                        "user": {
+                            "name": self.username,
+                            "password": self.password,
+                            "domain": {"id": self.project_id},
+                        },
+                    },
+                },
+                "scope": {
+                    "project": {
+                        "name": self.project_id,
+                        "domain": {"id": self.project_id},
+                    }
+                },
+            },
+        }
+        resp = requests.post(
+            self.keystone_token_endpoint, headers=self.headers, json=data)
+        resp.raise_for_status()
+        self.assertValidTokenResponse(resp)
