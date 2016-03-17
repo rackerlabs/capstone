179a91e8cfd7f56d5d290f9502aab101db125bb8
David Stanek <dstanek@dstanek.com>
2016-03-17 19:15:24 +0000

    User scope shortcut
    If the user's scope appears in the list of roles (as a project id) then
    it is safe to assume they are indeed a member of that domain.
    
    Change-Id: I056bedcf4eac7bb456e5d00d29f86c49c344c7ae

--- capstone/tests/unit/test_auth.py
+++ capstone/tests/unit/test_auth.py
@@ -28,6 +28,110 @@ class TestRackspaceIdentity(testtools.TestCase):
         self.password = uuid.uuid4().hex
         self.domain = uuid.uuid4().hex
 
+    def test_correct_user_domain_id_from_roles(self):
+        """Check the user's domain using the project IDs from the roles."""
+        token_data = {
+            'access': {
+                'user': {
+                    'id': uuid.uuid4().hex,
+                    'roles': [
+                        {'tenantId': uuid.uuid4().hex},
+                        {'tenantId': self.domain},
+                    ]
+                }
+            }
+        }
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_id=self.domain)
+        identity._assert_user_domain(token_data)
+
+    def test_correct_user_domain_name_from_roles(self):
+        """Check the user's domain using the project IDs from the roles."""
+        token_data = {
+            'access': {
+                'user': {
+                    'id': uuid.uuid4().hex,
+                    'roles': [
+                        {'tenantId': uuid.uuid4().hex},
+                        {'tenantId': self.domain},
+                    ]
+                }
+            }
+        }
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_name=self.domain)
+        identity._assert_user_domain(token_data)
+
+    def test_correct_user_domain_id_from_Rackspace(self):
+        """Check the user's domain using the data provided by Rackspace."""
+        token_data = {
+            'access': {
+                'user': {
+                    'id': uuid.uuid4().hex,
+                    'roles': [
+                        {'tenantId': uuid.uuid4().hex},
+                        {'tenantId': uuid.uuid4().hex},
+                    ]
+                }
+            }
+        }
+        user_data = {
+            const.RACKSPACE_DOMAIN_KEY: self.domain,
+        }
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_id=self.domain)
+        with mock.patch.object(auth_plugin.RackspaceIdentity,
+                               'from_admin_config') as m:
+            m().get_user.return_value = user_data
+            identity._assert_user_domain(token_data)
+
+    def test_correct_user_domain_name_from_Rackspace(self):
+        """Check the user's domain using the data provided by Rackspace."""
+        token_data = {
+            'access': {
+                'user': {
+                    'id': uuid.uuid4().hex,
+                    'roles': [
+                        {'tenantId': uuid.uuid4().hex},
+                        {'tenantId': uuid.uuid4().hex},
+                    ]
+                }
+            }
+        }
+        user_data = {
+            const.RACKSPACE_DOMAIN_KEY: self.domain,
+        }
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_name=self.domain)
+        with mock.patch.object(auth_plugin.RackspaceIdentity,
+                               'from_admin_config') as m:
+            m().get_user.return_value = user_data
+            identity._assert_user_domain(token_data)
+
+    def test_incorrect_user_domain(self):
+        token_data = {
+            'access': {
+                'user': {
+                    'id': uuid.uuid4().hex,
+                    'roles': [
+                        {'tenantId': uuid.uuid4().hex},
+                        {'tenantId': uuid.uuid4().hex},
+                    ]
+                }
+            }
+        }
+        user_data = {
+            const.RACKSPACE_DOMAIN_KEY: uuid.uuid4().hex,
+        }
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, scope_domain_id=self.domain)
+        with mock.patch.object(auth_plugin.RackspaceIdentity,
+                               'from_admin_config') as m:
+            m().get_user.return_value = user_data
+            self.assertRaises(exception.Unauthorized,
+                              identity._assert_domain_scope,
+                              token_data)
+
     def test_correct_domain_scope_from_roles(self):
         """Check the domain scope using the project IDs from the roles."""
         token_data = {
