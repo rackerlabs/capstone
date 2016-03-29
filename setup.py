6e52376e6e95cdcc6924ab21e2215690cb6c9c03
Jorge Munoz <elkidster@gmail.com>
2016-03-29 23:14:09 +0000

    Cache invalidator
    Add a procees to invalidate capstone's token cache by reading
    Rackspace's identity events feeds.
    
    Change-Id: Ib7edbbbb7075b1c20a25c9fac14882de6a72c70f

--- setup.py
+++ setup.py
@@ -70,6 +70,9 @@ setuptools.setup(
         'keystone.token.provider': [
             'capstone = capstone.token_provider:Provider',
         ],
+        'capstone.cache_invalidator': [
+            'capstone = capstone.cache_invalidator:main'
+        ],
     },
     install_requires=read_requirements('requirements.txt')
 )
