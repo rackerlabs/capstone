f31ffc708d722e798f41cf536e092f1361364a70
Jorge Munoz <elkidster@gmail.com>
2016-03-29 23:14:09 +0000

    Cache invalidator
    Add a procees to invalidate capstone's token cache by reading
    Rackspace's identity events feeds.
    
    Change-Id: Ib7edbbbb7075b1c20a25c9fac14882de6a72c70f

---
+++ capstone/cache_invalidator.py
@@ -0,0 +1,171 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import sched
+import time
+
+from keystone.i18n import _LI
+from oslo_log import log
+import requests
+
+from capstone import conf
+from capstone import const
+
+
+CONF = conf.CONF
+LOG = log.getLogger(__name__)
+
+
+class Entry(object):
+    """Entry for invalidation."""
+
+    def __init__(self, entry):
+        self._entry = entry
+
+    @property
+    def type(self):
+        return self._entry['content']['event']['type']
+
+    @property
+    def resource_id(self):
+        return self._entry['content']['event']['resourceId']
+
+    @property
+    def resource_type(self):
+        return self._entry['content']['event']['product']['resourceType']
+
+    def invalidate(self):
+        """Invalidate entry.
+
+        Invalidate capstone's cache by event type.
+        """
+        if self.resource_type in ['USER', 'TRR_USER']:
+            # Invalidate all tokens for updated/deleted user
+            if self.type in ['UPDATE', 'SUSPEND', 'DELETE']:
+                user_id = self.resource_id
+                LOG.info(_LI('Invalidate tokens for updated/deleted user %s'),
+                         user_id)
+        elif self.resource_type == 'TOKEN':
+            # Invalidate token by id
+            if self.type == 'DELETE':
+                token_id = self.resource_id
+                LOG.info(_LI('Invalidate token %s'), token_id)
+
+
+def get_admin_token(retry=True):
+    """Authentication request to v2.
+
+    Make a v2 authentication request to retrieve an admin token. Retry on
+    first faliure.
+    """
+    data = {
+        "auth": {
+            "passwordCredentials": {
+                "username": CONF.service_admin.username,
+                "password": CONF.service_admin.password,
+            },
+        },
+    }
+    token_url = CONF.rackspace.base_url + '/tokens'
+    resp = requests.post(token_url, headers=const.HEADERS, json=data)
+    if resp.status_code == requests.codes.ok:
+        token_data = resp.json()
+        return token_data['access']['token']['id']
+    elif retry:
+        return get_admin_token(False)
+    else:
+        msg = (_LI('Authentication failed against v2: response code %s') %
+               resp.status_code)
+        LOG.info(msg)
+        raise RuntimeError(msg)
+
+
+class CloudFeedClient(object):
+    """Client to parse identity event feeds."""
+
+    def __init__(self, feed_url):
+        self._feed_url = feed_url
+        self._marker = None
+        self._headers = {'Accept': 'application/vnd.rackspace.atom+json'}
+        self._admin_token = get_admin_token()
+        self._entries = []
+
+    def _page_feed(self, session, url, retry=True):
+        """Page event feed(s).
+
+        This method will read all events and add them to a list. This process
+        of paging will continue until no previous url is returned in the links
+        header.
+
+        :param session: request session
+        :param url: feed endpoint
+        :param retry: boolean used for single re-authentication
+
+        :raise SystemExit: If response from feed is not 200 OK and
+        re-authentication failed.
+        """
+        self._headers['X-Auth-Token'] = self._admin_token
+        resp = session.get(url, headers=self._headers)
+        if resp.status_code == requests.codes.ok:
+            feed = json.loads(resp.content)['feed']
+            if 'entry' in feed:
+                self._entries = list(feed['entry']) + self._entries
+            if 'previous' in resp.links:
+                self._page_feed(session, resp.links['previous']['url'])
+        elif resp.status_code == requests.codes.unauthorized and retry:
+            self._admin_token = get_admin_token()
+            LOG.info(_LI('Authorization failed: Re-authenticating'))
+            self._page_feed(session, url, False)
+        else:
+            msg = (_LI('Failed to parse feed: response code %s') %
+                   resp.status_code)
+            LOG.info(msg)
+            raise RuntimeError(msg)
+
+    def _parse(self):
+        """Parse events.
+
+        This method will set and use an event id marker to denote the last
+        event parsed.
+        """
+        endpoint = (self._feed_url +
+                    ("?marker=" + self._marker if self._marker else ""))
+        self._entries = []
+        s = requests.Session()
+        self._page_feed(s, endpoint)
+        if self._entries:
+            self._marker = self._entries[0]['id']
+
+    @property
+    def entries(self):
+        self._parse()
+        LOG.info(_LI("Parsing %d event feed(s)"), len(self._entries))
+        return self._entries
+
+
+def start(sc, client):
+    for entry in client.entries:
+        event = Entry(entry)
+        event.invalidate()
+    sc.enter(int(CONF.rackspace.polling_period), 1, start, (sc, client))
+
+
+def main():
+    try:
+        client = CloudFeedClient(CONF.rackspace.feed_url)
+        sc = sched.scheduler(time.time, time.sleep)
+        # Method enter(delay, priority, action, argument)
+        sc.enter(0, 1, start, (sc, client))
+        sc.run()
+    except RuntimeError as e:
+        raise SystemExit(e.message)
---
+++ capstone/tests/unit/test_cache_invalidator.py
@@ -0,0 +1,94 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import os
+
+from oslo_config import cfg
+from oslo_config import fixture as config_fixture
+import requests_mock
+import testtools
+
+from capstone import cache_invalidator
+
+
+CONF = cfg.CONF
+
+FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+
+class TestCloudFeedClient(testtools.TestCase):
+
+    def config_files(self):
+        return []
+
+    def config_overrides(self):
+        self.config_fixture.config(group='rackspace',
+                                   base_url=self.example_url)
+        self.config_fixture.config(group='rackspace',
+                                   feed_url=self.example_url)
+        self.config_fixture.config(group='rackspace',
+                                   polling_period=60)
+        self.config_fixture.config(group='service_admin',
+                                   username='username')
+        self.config_fixture.config(group='service_admin',
+                                   password='password')
+
+    def config(self, config_files):
+        CONF(args=[], project='capstone', default_config_files=config_files)
+
+    @requests_mock.mock()
+    def setUp(self, m):
+        super(TestCloudFeedClient, self).setUp()
+        self.example_url = 'http://example.com'
+        self.tokens_url = 'http://example.com/tokens'
+        self.config_fixture = self.useFixture(config_fixture.Config(CONF))
+        self.config(self.config_files())
+        self.config_overrides()
+        m.post(requests_mock.ANY, status_code=200,
+               json={"access": {"token": {"id": "id"}}})
+        self.ci = cache_invalidator.CloudFeedClient(self.example_url)
+        filename = os.path.join(FIXTURES_DIR, 'rackspace_feed_response.json')
+        with open(filename) as f:
+            self.feed_response_body = json.load(f)
+
+    @requests_mock.mock()
+    def test_feed_client_parse_correct_number_of_feed_events(self, m):
+        m.get(self.example_url, content=json.dumps(self.feed_response_body))
+        self.assertEqual(len(self.ci.entries), 3)
+
+    @requests_mock.mock()
+    def test_feed_client_set_correct_marker(self, m):
+        m.get(self.example_url, content=json.dumps(self.feed_response_body))
+        entries = self.ci.entries
+        self.assertEqual(self.ci._marker, entries[0]['id'])
+
+    @requests_mock.Mocker()
+    def test_feed_client_reauthentication_on_unauthorized(self, m):
+        m.register_uri('GET', self.example_url,
+                       [{'status_code': 401},
+                        {'content': json.dumps(self.feed_response_body),
+                         'status_code': 200}])
+        m.register_uri('POST', self.tokens_url, status_code=200,
+                       json={"access": {"token": {"id": "id"}}})
+        self.assertEqual(len(self.ci.entries), 3)
+
+    @requests_mock.mock()
+    def test_feed_client_fails_after_single_reauthentication_failure(self, m):
+        m.get(self.example_url, status_code=401)
+        m.post(self.tokens_url, status_code=200,
+               json={"access": {"token": {"id": "id"}}})
+        try:
+            self.ci.entries
+            assert False
+        except RuntimeError:
+            pass
