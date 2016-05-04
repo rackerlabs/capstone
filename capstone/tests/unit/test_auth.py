b068b326502fc915942ce91d11b15c4701b874d3
nonameentername <nonameentername@gmail.com>
2016-05-04 14:54:44 -0500

    Add X-Forwarded-For header
    This will allow ops to debug a little better or possibly filter with
    Repoze.
    
    Change-Id: I9bf065b0d1c2e7b3d37edd2b594a5ac75f97c065

--- capstone/tests/unit/test_auth.py
+++ capstone/tests/unit/test_auth.py
@@ -16,6 +16,7 @@ from keystone import exception
 import testtools
 
 from capstone import auth_plugin
+from capstone import const
 
 
 class TestRackspaceIdentity(testtools.TestCase):
@@ -167,3 +168,29 @@ class TestRackspaceIdentity(testtools.TestCase):
         identity._user_ref = {'RAX-AUTH:domainId': uuid.uuid4().hex}
         self.assertRaises(exception.Unauthorized,
                           identity._assert_domain_scope, token_data)
+
+    def test_update_headers_with_x_forwarded_for_header(self):
+        headers = const.HEADERS.copy()
+
+        context = {
+            'environment': {
+                'HTTP_HOST': '127.0.0.1',
+                'SERVER_NAME': '127.0.0.2'
+            }
+        }
+
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_name=self.domain)
+        identity._update_headers_with_x_forwarded_for_header(headers, context)
+
+        self.assertIn('X-Forwarded-For', headers)
+        self.assertEqual(headers['X-Forwarded-For'], '127.0.0.1, 127.0.0.2')
+
+    def test_update_headers_with_x_forwarded_for_header_without_context(self):
+        headers = const.HEADERS.copy()
+
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_name=self.domain)
+        identity._update_headers_with_x_forwarded_for_header(headers)
+
+        self.assertNotIn('X-Forwarded-For', headers)
