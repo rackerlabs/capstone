5dfcc216989b705c619112e0db47f5f81fa8db84
Dolph Mathews <dolph.mathews@gmail.com>
2016-05-04 11:07:29 -0500

    Upgrade keystone to stable/mitaka
    Change-Id: I2343a35df743b5f5c7ceb65b564d117c7dfe6499

--- capstone/token_provider.py
+++ capstone/token_provider.py
@@ -213,6 +213,12 @@ class Provider(common.BaseProvider):
         finally:
             self.v3_token_data_helper = None
 
+    def validate_non_persistent_token(self, token_id):
+        raise exception.TokenNotFound('The requested operation is forbidden '
+                                      'because token validation is not '
+                                      'supported by the v3 API; use v2.0 '
+                                      'instead.')
+
     def validate_v3_token(self, token_ref):
         # TODO(lbragstad): This should be changed to use
         # exception.ForbiddenNotSecurity() with a detailed error message once
