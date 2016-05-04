4b5c8cbd1259f6151176d925d40c217851591d41
Dolph Mathews <dolph.mathews@gmail.com>
2016-05-04 15:55:41 -0500

    Switch to ForbiddenNotSecurity
    This is a new exception class introduced by Keystone in stable/mitaka,
    which we can now take advantage of.
    
    Change-Id: I638a132ee1714ccac3f3ad9a7b38ffb5c1df188a

--- capstone/token_provider.py
+++ capstone/token_provider.py
@@ -219,7 +219,6 @@ class Provider(common.BaseProvider):
             ' not supported by the v3 API; use v2.0 instead.')
 
     def validate_v3_token(self, token_ref):
-        # TODO(lbragstad): This should be changed to use
-        # exception.ForbiddenNotSecurity() with a detailed error message once
-        # we switch to using stable/mitaka.
-        raise exception.Forbidden()
+        raise exception.ForbiddenNotSecurity(
+            'The requested operation is forbidden because token validation is'
+            ' not supported by the v3 API; use v2.0 instead.')
