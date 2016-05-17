b7f83a4ca6eb706f50233c78ef3099fc853002ba
Jorge Munoz <elkidster@gmail.com>
2016-05-17 21:53:18 +0000

    Test v2 outages
    Capstone should fail gracefully if an error occurs connecting to v2.
    
    Change-Id: I1ed926084b60b3f2f6f0153fcbca8dc9a61f25ae

---
+++ capstone/tests/unit/test_v2_outages.py
@@ -0,0 +1,126 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid
+
+from keystone import exception
+import requests_mock
+import testtools
+
+from capstone import auth_plugin
+
+
+class TestV2Outages(testtools.TestCase):
+
+    def setUp(self):
+        super(TestV2Outages, self).setUp()
+        self.username = uuid.uuid4().hex
+        self.user_id = uuid.uuid4().hex
+        self.password = uuid.uuid4().hex
+        self.domain_id = uuid.uuid4().hex
+        self.token = uuid.uuid4().hex
+
+    def _get_token_data(self):
+        return {
+            "access": {
+                "token": {
+                    "id": self.token,
+                },
+                "user": {
+                    "name": self.username,
+                    "id": self.user_id,
+                    "RAX-AUTH:domainId": self.domain_id,
+                    "roles": [
+                        {
+                            "id": "id",
+                        },
+                    ],
+                },
+            },
+        }
+
+    def _get_user(self):
+        return {
+            "user": {
+                "id": self.user_id,
+                "username": self.username,
+                "RAX-AUTH:domainId": self.domain_id
+            }
+        }
+
+    @requests_mock.mock()
+    def test_v2_get_user_timeout(self, m):
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_id=self.domain_id)
+        user_url = identity.url('/users/%s' % self.user_id)
+
+        m.get(user_url, status_code=408)
+        m.post(identity.url('/tokens'), status_code=200,
+               json=self._get_token_data())
+
+        self.assertRaises(exception.Error,
+                          identity.get_user, self.user_id)
+
+    @requests_mock.mock()
+    def test_v2_get_user_bad_gateway(self, m):
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_id=self.domain_id)
+        user_url = identity.url('/users/%s' % self.user_id)
+
+        m.get(user_url, status_code=502)
+        m.post(identity.url('/tokens'), status_code=200,
+               json=self._get_token_data())
+
+        self.assertRaises(exception.Error,
+                          identity.get_user, self.user_id)
+
+    @requests_mock.mock()
+    def test_v2_get_user_by_name_timeout(self, m):
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_id=self.domain_id)
+
+        user_url = identity.url('/users?name=%s' % self.username)
+        m.get(user_url, status_code=408)
+        m.post(identity.url('/tokens'), status_code=200,
+               json=self._get_token_data())
+        self.assertRaises(exception.Error,
+                          identity.get_user_by_name, self.username)
+
+    @requests_mock.mock()
+    def test_v2_authenticate_bad_gateway(self, m):
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_id=self.domain_id)
+
+        m.post(identity.url('/tokens'), status_code=502)
+        self.assertRaises(exception.Error, identity.authenticate)
+
+    @requests_mock.Mocker()
+    def test_v2_authentication_with_intermittent_failures(self, m):
+        identity = auth_plugin.RackspaceIdentity.from_username(
+            self.username, self.password, user_domain_id=self.domain_id)
+
+        user_url = identity.url('/users?name=%s' % self.username)
+        m.register_uri('GET', user_url, status_code=200,
+                       json=self._get_user())
+        m.register_uri('POST', identity.url('/tokens'),
+                       [{'status_code': 200, 'json': self._get_token_data()},
+                        {'status_code': 502},
+                        {'status_code': 200, 'json': self._get_token_data()},
+                        {'status_code': 408}])
+
+        token_data = identity.authenticate()
+        self.assertIsNotNone(token_data)
+        self.assertRaises(exception.Error, identity.authenticate)
+
+        token_data = identity.authenticate()
+        self.assertIsNotNone(token_data)
+        self.assertRaises(exception.Error, identity.authenticate)
