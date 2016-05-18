26765865d1a98995fa56f3da441de3695e824c4c
Dolph Mathews <dolph.mathews@gmail.com>
2016-05-18 15:03:57 -0500

    Replaces Flask with wsgiref for the mock service
    This removes all external dependencies. Everything the service needs is
    in the Python standard library.
    
        $ python capstone/tests/integration/mock_v2/service.py
        $ curl http://localhost:8000/v2.0/tokens --data '{"auth": {"passwordCredentials": {"username": "test", "password": "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"}}}' -H "Content-Type: application/json"
    
    Change-Id: I73bc3b06fc9024af0f280b8954d649d368b06242

--- capstone/tests/integration/mock_v2/service.py
+++ capstone/tests/integration/mock_v2/service.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """Mock implementation of v2.
 
 This module provides a real web service that you can use perform real
@@ -12,13 +14,17 @@ and tenant names, and a user's tenancy will match their domain ID.
 
 import datetime
 import hashlib
+import json
+import re
+import sys
+import urlparse  # six urllib maybe?
 import uuid
-
-import flask
-from flask import request
+import wsgiref.simple_server
 
 
-app = flask.Flask('v2')
+# content type headers
+PLAIN_TEXT = ('Content-type', 'plain/text')
+APP_JSON = ('Content-type', 'application/json')
 
 TOKEN_CACHE = dict()
 
@@ -28,30 +34,17 @@ def hash_str(*args):
     return hashlib.sha1(''.join(args)).hexdigest()
 
 
-def unauthorized():
-    """Return a 401 Unauthorized response."""
-    response = flask.json.jsonify(**{
-        "unauthorized": {
-            "message": "Unable to authenticate user with credentials"
-                       " provided.",
-            "code": 401
-        }
-    })
-    response.status_code = 401
-    return response
-
-
-@app.route('/v2.0/users', methods=['GET'])
-def list_users():
+def list_users(environ, start_response):
     """List users (but really get user by name)."""
-    username = request.args.get('name')
+    qs = urlparse.parse_qs(environ.get('QUERY_STRING', ''))
+    username = qs.get('name', [''])[0]
 
     # We don't actually have a reason to implement list users, just get user by
     # name.
     if not username:
         raise NotImplementedError('List users not supported.')
 
-    return flask.json.jsonify(**{
+    data = json.dumps({
         "user": {
             "RAX-AUTH:domainId": hash_str('account_id', username),
             "username": username,
@@ -63,31 +56,46 @@ def list_users():
             "id": username,
         }
     })
+    start_response('200 OK', [APP_JSON])
+    return [data]
 
 
-@app.route('/v2.0/users/<user_id>', methods=['GET'])
-def get_user_by_id(user_id):
-    return flask.json.jsonify(**{
+def get_user(environ, start_response):
+    user_id = environ['wsgi.match'].groups()[0]
+
+    data = json.dumps({
         "user": {
             "RAX-AUTH:domainId": hash_str('account_id', user_id),
             "username": user_id,
             "enabled": True,
             "email": "%s@example.com" % user_id,
             "RAX-AUTH:defaultRegion": "IAD",
+            "RAX-AUTH:multiFactorEnabled": False,
             "id": user_id,
         }
     })
+    start_response('200 OK', [APP_JSON])
+    return [data]
 
 
-@app.route('/v2.0/tokens', methods=['POST'])
-def authenticate():
-    username = request.json['auth']['passwordCredentials']['username']
-    password = request.json['auth']['passwordCredentials']['password']
+def authenticate(environ, start_response):
+    content_len = int(environ['CONTENT_LENGTH'])
+    request_body = json.loads(environ['wsgi.input'].read(content_len))
+    username = request_body['auth']['passwordCredentials']['username']
+    password = request_body['auth']['passwordCredentials']['password']
 
     # Authentication is valid if the password is the SHA1 hexdigest of the
     # username.
     if hash_str(username) != password:
-        return unauthorized()
+        data = json.dumps({
+            "unauthorized": {
+                "message": "Unable to authenticate user with credentials"
+                           " provided.",
+                "code": 401
+            }
+        })
+        start_response('401 Unauthorized', [APP_JSON])
+        return [data]
 
     token_id = uuid.uuid4().hex
     tenant_id = hash_str('account_id', username)
@@ -95,7 +103,7 @@ def authenticate():
     five_minutes = datetime.timedelta(minutes=5)
     expires = datetime.datetime.utcnow() + five_minutes
 
-    TOKEN_CACHE[token_id] = {
+    data = json.dumps({
         "access": {
             "serviceCatalog": [
                 {
@@ -218,16 +226,74 @@ def authenticate():
                 ]
             }
         }
-    }
+    })
+    start_response('200 OK', [APP_JSON])
+    return [data]
 
-    return flask.json.jsonify(**TOKEN_CACHE[token_id])
 
-
-@app.route('/v2.0/tokens/<token_id>', methods=['GET'])
-def validate(token_id):
-    if token_id not in TOKEN_CACHE:
-        return unauthorized()
-
+def validate(environ, start_response):
     # TODO(dstanek): maybe have this check expiration if
     # we have tests that depend on this
-    return flask.json.jsonify(**TOKEN_CACHE[token_id])
+    token_id = environ['wsgi.match'].groups()[0]
+    if token_id not in TOKEN_CACHE:
+        status = '401 Unauthorized'
+        data = json.dumps({
+            "unauthorized": {
+                "message": "Unable to authenticate user with credentials"
+                           " provided.",
+                "code": 401
+            }
+        })
+    else:
+        status = '200 OK'
+        # TODO(dstanek): figure out what this should be
+        data = json.dumps({'msg': 'woot'})
+
+    start_response(status, [APP_JSON])
+    return [data]
+
+
+def not_found(environ, start_response):
+    """Micro application returning a 404 and all the environment vars.
+
+    This is useful for debugging. Just hit a url that doesn't exist,
+    like '/', and see what is getting setup.
+    """
+    start_response('404 Not Found', [PLAIN_TEXT])
+    return ["%s: %s\n" % (key, value) for key, value in environ.iteritems()]
+
+
+def application(environ, start_response):
+    """Main application entrypoint."""
+    for (method, pattern), micro_app in routes:
+        match = matches(method, pattern, environ)
+        if match:
+            environ['wsgi.match'] = match
+            return micro_app(environ, start_response)
+    return not_found(environ, start_response)
+
+
+def matches(method, pattern, environ):
+    if environ['REQUEST_METHOD'] != method:
+        return None
+    return re.match(pattern, environ['PATH_INFO'])
+
+
+routes = (
+    (('GET', r'^/v2.0/users/(\w+)$'), get_user),
+    (('GET', r'^/v2.0/users/?$'), list_users),
+    (('POST', r'^/v2.0/tokens/?$'), authenticate),
+    (('GET', r'^/v2.0/tokens/(\w+)$'), validate),
+)
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except IndexError:
+        port = 8000
+    httpd = wsgiref.simple_server.make_server('', port, application)
+    print("Serving on port %s..." % port)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
