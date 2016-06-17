8af5b24722bbd88956ad9a6353d25c06c1e68ecf
Jorge Munoz <elkidster@gmail.com>
2016-06-17 19:38:45 +0000

    Implement token authentication
    Allow token authentication request for capstone.
    
    Change-Id: I91ee47d5edad1b93e04d542eca7b4c5df7948b69

--- capstone/tests/integration/test_integration.py
+++ capstone/tests/integration/test_integration.py
@@ -130,6 +130,20 @@ def generate_password_auth_data_with_scope(user, scope):
     }
 
 
+def generate_token_auth_data_with_scope(token_id, scope):
+    return {
+        "auth": {
+            "identity": {
+                "methods": ["token"],
+                "token": {
+                    "id": token_id,
+                },
+            },
+            "scope": scope,
+        },
+    }
+
+
 class TestGettingADefaultScopedToken(BaseIntegrationTests):
 
     def test_with_username(self):
@@ -414,6 +428,136 @@ class TestGettingADomainScopedToken(BaseIntegrationTests):
             },
             scope={"domain": {"id": uuid.uuid4().hex}})
         self.authenticate(data, httplib.UNAUTHORIZED)
+
+
+class TestTokenAuthentication(BaseIntegrationTests):
+
+    def test_with_project_id(self):
+        data = generate_password_auth_data({
+            "name": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = generate_token_auth_data_with_scope(
+            token_id=token,
+            scope={"project": {"id": self.project_id}})
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+        self.assertTokenIsUseable(token)
+        self.assertValidTokenResponse(resp)
+
+    def test_with_project_name(self):
+        data = generate_password_auth_data({
+            "name": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = generate_token_auth_data_with_scope(
+            token_id=token,
+            scope={"project": {"name": self.project_id}})
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+        self.assertTokenIsUseable(token)
+        self.assertValidTokenResponse(resp)
+
+    def test_with_invalid_project_id(self):
+        data = generate_password_auth_data({
+            "name": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = generate_token_auth_data_with_scope(
+            token_id=token,
+            scope={"project": {"id": 'invalid'}})
+        self.authenticate(data, httplib.UNAUTHORIZED)
+
+    def test_with_empty_project_id(self):
+        data = generate_password_auth_data({
+            "name": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = generate_token_auth_data_with_scope(
+            token_id=token,
+            scope={"project": {"id": ""}})
+        self.authenticate(data, httplib.BAD_REQUEST)
+
+    def test_with_domain_id(self):
+        data = generate_password_auth_data({
+            "name": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = generate_token_auth_data_with_scope(
+            token_id=token,
+            scope={"domain": {"id": self.project_id}})
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+        self.assertTokenIsUseable(token)
+        self.assertValidTokenResponse(resp)
+
+    def test_with_invalid_domain_id(self):
+        data = generate_password_auth_data({
+            "name": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = generate_token_auth_data_with_scope(
+            token_id=token,
+            scope={"domain": {"id": 'invalid'}})
+        self.authenticate(data, httplib.UNAUTHORIZED)
+
+    def test_with_empty_domain_id(self):
+        data = generate_password_auth_data({
+            "name": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = generate_token_auth_data_with_scope(
+            token_id=token,
+            scope={"domain": {"id": ""}})
+        self.authenticate(data, httplib.BAD_REQUEST)
+
+    def test_with_no_scope(self):
+        data = generate_password_auth_data({
+            "id": self.username,
+            "password": self.password,
+        })
+        resp = self.authenticate(data)
+        token = resp.headers['X-Subject-Token']
+
+        data = {
+            "auth": {
+                "identity": {
+                    "methods": ["token"],
+                    "token": {
+                        "id": token,
+                    },
+                },
+            },
+        }
+
+        self.authenticate(data, httplib.BAD_REQUEST)
+
+    def test_with_empty_token_id(self):
+        data = generate_token_auth_data_with_scope(
+            token_id='',
+            scope={"project": {"id": self.project_id}})
+        self.authenticate(data, httplib.BAD_REQUEST)
 
 
 class TestCapstonePolicy(BaseIntegrationTests):
